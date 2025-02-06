8c5c01a8af0af4ef7968383e97c5f9e0dc15b52b
ThanarakNK <tanarak.n@airroot.asia>
2025-02-06 16:26:06 +0700

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,  
-    host: '0.0.0.0',  
-  },
 })
